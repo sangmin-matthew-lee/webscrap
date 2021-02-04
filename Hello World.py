a64531de5452e861361c6f062d8f6d6b9ffff000
Sangmin Matthew <sangmin.lee@sjsu.edu>
2021-02-04 11:01:36 +0900

    Hello World and Calculator

---
+++ Hello World.py
@@ -0,0 +1,6 @@
+
+def greeting(name):
+    print(f"Hello {name}, Welcome to Python!!!")
+
+name = input("What is your name?")
+greeting(name);
---
+++ calculator.py
@@ -0,0 +1,29 @@
+
+def plus(a,b):
+    print(a + b)
+
+def sub(a,b):
+    print(a-b)
+
+def multiple(a,b):
+    print(a*b)
+
+def div (a,b):
+    print(a/b)
+
+def pow(a,b):
+    print(a**b)
+
+def rem(a,b):
+    print(a%b)
+
+def neg(a):
+    print(-a)
+
+plus(2,5)
+sub(5,3)
+multiple(2,5)
+div(2,2)
+pow(2,5)
+rem(3,2)
+neg(5)
